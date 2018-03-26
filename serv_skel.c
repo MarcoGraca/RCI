13c6a8889adf7bf5cc456ade5b87c5d3621a8ccf
Marco Graça <marcostbgr16@gmail.com>
2018-03-26 17:17:40 +0100

    central contact

--- serv_skel.c
+++ serv_skel.c
@@ -7,6 +7,7 @@
 #include <netdb.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
 #define PORT 59000
 #define SERVER_ID "-n"
 #define SERVER_IP "-j"
@@ -16,24 +17,56 @@
 #define CSERVER_PORT "-p"
 #define SERV_OK 1
 #define SERV_TROUBLE 0
+#define JOINING_DISPATCH "MY SERVICE ON"
+#define JOINED_DISPATCH "YOUR SERVICE ON"
+#define LEAVING_DISPATCH "MY SERVICE OFF"
+#define LEFT_DISPATCH "YOUR SERVICE OFF"
+#define BUFFERSIZE 50
 #define max(A,B) ((A)>=(B)?(A):(B))
+
+int central_contact(char *msg, struct sockaddr_in *c_serveraddr, int fd, char *buffer)
+{
+  socklen_t addrlen = sizeof(c_serveraddr);
+  int sent_bytes, recv_bytes, count;
+  struct timeval countdwn;
+  countdwn.tv_sec=3;
+  countdwn.tv_usec=0;
+  fd_set rfds;
+  sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&c_serveraddr, addrlen);
+  if (sent_bytes == -1)
+  {
+    perror("Error: ");
+    return(SERV_TROUBLE);
+  }
+  FD_ZERO(&rfds);
+  FD_SET(fd,&rfds);
+  count=select(fd+1,&rfds,(fd_set*)NULL,(fd_set*)NULL,&countdwn);
+  if (count < 1 || countdwn.tv_sec==0)
+    return 0;
+  addrlen = sizeof(c_serveraddr);
+  recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&c_serveraddr, &addrlen);
+  if (recv_bytes == -1)
+  {
+    perror("Error: ");
+    return(SERV_TROUBLE);
+  }
+  return(SERV_OK);
+}
 
 int main(int argc, char *argv[])
 {
   printf("GOT HERE\n");
-  int fd, clfd, recv_bytes, sent_bytes, port, arg_count = 0, myid, start_id, id, service, state, myip_arg, myTCPport_arg, myUDPport_arg, counter, maxfd;
+  int i,fd, clfd, recv_bytes, sent_bytes, port, arg_count = 0, myid, start_id, id, service, state, myip_arg, myTCPport_arg, myUDPport_arg, counter, maxfd;
   fd_set rfds, wfds;
   socklen_t addrlen;
   struct sockaddr_in c_serveraddr, myudpaddr, mytcpaddr, clientaddr;
-  struct hostent *hostptr;
   printf("GOT HERE\n");
   struct in_addr ip;
-  char msg[50], buffer[50], req[50], command[20];
+  char msg[BUFFERSIZE], buffer[BUFFERSIZE], req[BUFFERSIZE], command[BUFFERSIZE];
   enum {idle, contact, on_ring, busy} status;
-  enum {ring_join, ring_wd, forced_ring_wd, client_request} cont;
+  enum {ring_join, ring_wd, client_join, client_wd} cont;
   fd = socket(AF_INET,SOCK_DGRAM,0);
   clfd = socket(AF_INET,SOCK_DGRAM,0);
-  printf("%d\n",fd);
   if (argc > 8 && argc % 2)
   {
     memset((void*)&c_serveraddr,(int)'\0', sizeof(c_serveraddr));
@@ -44,11 +77,11 @@ int main(int argc, char *argv[])
     mytcpaddr.sin_family = AF_INET;
 
     c_serveraddr.sin_family = AF_INET;
-    hostptr = gethostbyname("tejo.tecnico.ulisboa.pt");
-    c_serveraddr.sin_addr.s_addr = ((struct in_addr *)(hostptr->h_addr_list[0]))->s_addr;
+    inet_aton("193.136.138.142",&ip);
+    c_serveraddr.sin_addr = ip;
     c_serveraddr.sin_port = htons((u_short)PORT);
 
-    for (int i=1; i<argc; i+=2)
+    for (i=1; i<argc; i+=2)
     {
       if(strcmp(argv[i],SERVER_ID)==0)
       {
@@ -99,288 +132,202 @@ int main(int argc, char *argv[])
     printf("Wrong invocation\n");
     return 0;
   }
-
-  addrlen = sizeof(c_serveraddr);
-
-  bind(fd,(struct sockaddr*)&c_serveraddr, sizeof(c_serveraddr));
   status=idle;
-  maxfd=fd;
   while(1)
   {
     FD_ZERO(&rfds);
-    FD_ZERO(&wfds);
     FD_SET(0,&rfds);
-    FD_SET(1,&wfds);
     maxfd=0;
-    if (status == contact)
-    {
-      FD_SET(fd,&rfds);
-      FD_SET(fd+1,&wfds);
-      maxfd=fd;
-      if (cont == forced_ring_wd)
-      {
-        FD_SET(cfd,&rfds);
-        FD_SET(cfd+1,&wfds);
-        maxfd=max(cfd,fd);
-      }
-    }
     if (status == on_ring || status == busy)
     {
-      FD_SET(cfd,&rfds);
-      FD_SET(cfd+1,&wfds);
-      maxfd=cfd;
+      FD_SET(clfd,&rfds);
+      maxfd=clfd;
     }
-    counter=select(maxfd+1,&rfds,&wfds,(fd_set*)NULL,(struct timeval *)NULL);
+    counter=select(maxfd+1,&rfds,(fd_set*)NULL,(fd_set*)NULL,(struct timeval *)NULL);
     if (counter < 1)
       return 0;
-    if (FD_ISSET(0,&rfds)&&FD_ISSET(1,&wfds))
+    if (FD_ISSET(0,&rfds))
     {
       switch (status)
       {
-
         case idle:
-        if(fgets(req,strlen(req),stdin)==NULL)
-          break;
-        else
         {
-          sscanf(req, "%s %d\n", command, &service);
-          if (strcmp(command,"join"))
+          if(fgets(req,BUFFERSIZE,stdin)==NULL)
+            break;
+          else
           {
-            if(service)
+            sscanf(req, "%s %d\n", command, &service);
+            printf("Service: %d\n", service);
+            if (strcmp(command,"join"))
             {
-              status=contact;
-              cont=ring_join;
+              if(service)
+              {
+                status=contact;
+                cont=ring_join;
+              }
+              else
+                printf("Invalid service id\n");
+            }
+            else if (strcmp(command,"show_state"))
+            {
+              printf("TESTE\n");
+            }
+            else if (strcmp(command,"leave"))
+              printf("Not in a ring\n");
+            else if (strcmp(command,"exit"))
+            {
+              close(fd);
+              return 1;
             }
             else
-              printf("Invalid service id\n");
+              printf("Unrecognized command\n");
           }
-          else if (strcmp(command,"show_state"))
-          {
-            printf("TESTE\n");
-          }
-          else if (strcmp(command,"leave"))
-            printf("Not in a ring\n");
-          else if (strcmp(command,"exit"))
-          {
-            close(fd);
-            return 1;
-          }
-          else
-            printf("Unrecognized command\n");
         }
         case contact:
-        if (FD_ISSET(fd,&rfds)&&FD_ISSET(fd+1,&wfds))
         {
           switch (cont)
           {
-
             case ring_join:
-
-            sprintf(msg,"GET START %d;%d\n",service, myid);
-            sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&clientaddr, addrlen);
-            if (sent_bytes == -1)
             {
-              perror("Error: ");
-              state = SERV_TROUBLE;
-              break;
-            }
-            addrlen = sizeof(clientaddr);
-            recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&clientaddr, &addrlen);
-            if (recv_bytes == -1)
-            {
-              perror("Error: ");
-              state = SERV_TROUBLE;
-              break;
-            }
-            state=sscanf(buffer,"OK %d;%d;%s", &id, &start_id, msg);
-            if (state < 3 || id!=myid)
-            {
-              printf("Trouble contacting the central server\n");
-              state = SERV_TROUBLE;
-              break;
-            }
-            if (start_id)
-            {
-              inet_aton(strtok(msg, ";"),&ip);
-              port=atoi(msg);
-              //TCP part//
-            }
-            else
-            {
-              sprintf(msg, "SET_START %d;%d;%s;%d", service, myid, argv[myip_arg], atoi(argv[myTCPport_arg]));
-              sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&clientaddr, addrlen);
+              sprintf(msg,"GET START %d;%d\n",service, myid);
+              addrlen = sizeof(c_serveraddr);
+              sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&c_serveraddr, addrlen);
               if (sent_bytes == -1)
               {
                 perror("Error: ");
                 state = SERV_TROUBLE;
                 break;
               }
-              recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&clientaddr, &addrlen);
+              addrlen = sizeof(clientaddr);
+              recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&c_serveraddr, &addrlen);
               if (recv_bytes == -1)
               {
                 perror("Error: ");
                 state = SERV_TROUBLE;
                 break;
               }
-              sscanf(buffer, "OK %d;%*s\n", &id);
-              if (id!=myid)
+              state=sscanf(buffer,"OK %d;%d;%s", &id, &start_id, msg);
+              if (state < 3 || id!=myid)
               {
                 printf("Trouble contacting the central server\n");
                 state = SERV_TROUBLE;
                 break;
               }
-              sprintf(msg, "SET_DS %d;%d;%s;%d", service, myid, argv[myip_arg], atoi(argv[myUDPport_arg]));
-              sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&clientaddr, addrlen);
-              if (sent_bytes == -1)
+              if (start_id)
               {
-                perror("Error: ");
-                state = SERV_TROUBLE;
-                break;
+                inet_aton(strtok(msg, ";"),&ip);
+                port=atoi(msg);
+                //TCP part//
               }
-              recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&clientaddr, &addrlen);
-              if (recv_bytes == -1)
+              else
               {
-                perror("Error: ");
-                state = SERV_TROUBLE;
-                break;
+                sprintf(msg, "SET_START %d;%d;%s;%d", service, myid, argv[myip_arg], atoi(argv[myTCPport_arg]));
+                sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&c_serveraddr, addrlen);
+                if (sent_bytes == -1)
+                {
+                  perror("Error: ");
+                  state = SERV_TROUBLE;
+                  break;
+                }
+                recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&clientaddr, &addrlen);
+                if (recv_bytes == -1)
+                {
+                  perror("Error: ");
+                  state = SERV_TROUBLE;
+                  break;
+                }
+                sscanf(buffer, "OK %d;%*s\n", &id);
+                if (id!=myid)
+                {
+                  printf("Trouble contacting the central server\n");
+                  state = SERV_TROUBLE;
+                  break;
+                }
+                sprintf(msg, "SET_DS %d;%d;%s;%d", service, myid, argv[myip_arg], atoi(argv[myUDPport_arg]));
+                sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&c_serveraddr, addrlen);
+                if (sent_bytes == -1)
+                {
+                  perror("Error: ");
+                  state = SERV_TROUBLE;
+                  break;
+                }
+                recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&c_serveraddr, &addrlen);
+                if (recv_bytes == -1)
+                {
+                  perror("Error: ");
+                  state = SERV_TROUBLE;
+                  break;
+                }
+                sscanf(buffer, "OK %d;%*s\n", &id);
+                if (id!=myid)
+                {
+                  printf("Trouble contacting the central server\n");
+                  state = SERV_TROUBLE;
+                  break;
+                }
+                status=on_ring;
               }
-              sscanf(buffer, "OK %d;%*s\n", &id);
-              if (id!=myid)
-              {
-                printf("Trouble contacting the central server\n");
-                state = SERV_TROUBLE;
-                break;
-              }
-              status=on_ring;
             }
             case ring_wd:
-            case forced_ring_wd:
-            case client_request:
+            {
+              break;
+            }
+            case client_join:
+            {
+              break;
+            }
+            case client_wd:
+            {
+              break;
+            }
           }
         }
         case on_ring:
         {
-          if(FD_ISSET(clfd,&rfds)&&FD_ISSET(clfd+1,&wfds))
+          if(fgets(req,BUFFERSIZE,stdin)==NULL)
           {
-            if(fgets(req,strlen(req),stdin)==NULL)
+            //TCP stuff
+            break;
+          }
+          else
+          {
+            sscanf(req, "%s\n", command);
+            if (strcmp(req,JOINING_DISPATCH)==0)
             {
-              //TCP stuff
-              break;
-            }
-            else
-            {
-              sscanf(req, "%s\n", command);
-              if (strcmp())
+              //TCP stuff here too probably
+              status=contact;
+              cont=client_join;
             }
           }
         }
         case busy:
-      }
-    }
-  }
-    /*}
-    if(fgets(req,strlen(req),stdin)!=NULL)
-    {
-      sscanf(req, "%s %d\n", command, &service);
-      if (strcmp(command,"join"))
-      {
-        if (service)
         {
-          sprintf(msg,"GET START %d;%d\n",service, myid);
-          sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&clientaddr, addrlen);
-          if (sent_bytes == -1)
+          if(FD_ISSET(clfd,&rfds)&&FD_ISSET(clfd+1,&wfds))
           {
-            perror("Error: ");
-            state = SERV_TROUBLE;
-            break;
-          }
-          addrlen = sizeof(clientaddr);
-          recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&clientaddr, &addrlen);
-          if (recv_bytes == -1)
-          {
-            perror("Error: ");
-            state = SERV_TROUBLE;
-            break;
-          }
-          state=sscanf(buffer,"OK %d;%d;%s", &id, &start_id, msg);
-
-          if (state < 3 || id!=myid)
-          {
-            printf("Trouble contacting the central server\n");
-            state = SERV_TROUBLE;
-            break;
-          }
-          if (start_id)
-          {
-            inet_aton(strtok(msg, ";"),&ip);
-            port=atoi(msg);
-            //TCP part//
-          }
-          else
-          {
-            sprintf(msg, "SET_START %d;%d;%s;%d", service, myid, argv[myip_arg], atoi(argv[myTCPport_arg]));
-            sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&clientaddr, addrlen);
-            if (sent_bytes == -1)
-            {
-              perror("Error: ");
-              state = SERV_TROUBLE;
-              break;
-            }
-            recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&clientaddr, &addrlen);
+            recv_bytes=recvfrom(clfd, buffer, sizeof(buffer),0, (struct sockaddr*)&clientaddr, &addrlen);
             if (recv_bytes == -1)
             {
               perror("Error: ");
               state = SERV_TROUBLE;
               break;
             }
-            sscanf(buffer, "OK %d;%*s\n", &id);
-            if (id!=myid)
+            if (strcmp(req,LEAVING_DISPATCH)==0)
             {
-              printf("Trouble contacting the central server\n");
-              state = SERV_TROUBLE;
-              break;
+              sprintf(msg, LEFT_DISPATCH);
+              sent_bytes=sendto(clfd, msg, strlen(msg)+1, 0, (struct sockaddr*)&clientaddr, addrlen);
+              if (sent_bytes == -1)
+              {
+                perror("Error: ");
+                state = SERV_TROUBLE;
+                break;
+              }
             }
-            sprintf(msg, "SET_DS %d;%d;%s;%d", service, myid, argv[myip_arg], atoi(argv[myTCPport_arg]));
-
           }
         }
-        else
-          printf("Invalid service id\n");
-        if (state == SERV_TROUBLE)
-          printf("Couldn't provide the service asked\n");
       }
-      else if (strcmp(command,"show_state"))
-      {
+    }
+    if (FD_ISSET(clfd,&rfds))
+    {
 
-      }
-      else if (strcmp(command,"leave"))
-      {
-
-      }
-      else if (strcmp(command,"exit"))
-      {
-        close(fd);
-        return 1;
-      }
-      else
-        printf("Unrecognized command\n");
     }
   }
-}
-//   }
-//   recv_bytes=recvfrom(fd, buffer, sizeof(buffer),0, (struct sockaddr*)&clientaddr, &addrlen);
-//   if (recv_bytes == -1)
-//   {
-//     perror("Error: ");
-//     return 0;
-//   }
-//   printf("Received: %s\n", buffer);
-//   strncpy(msg,strcat("Echo: ", buffer),strlen(msg));
-//   sent_bytes=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&clientaddr, addrlen);
-//   while(sent_bytes<strlen(msg)+1)
-//   {
-//     printf("Sent %d bytes\n", sent_bytes);
-//     sent_bytes+=sendto(fd, msg, strlen(msg)+1, 0, (struct sockaddr*)&clientaddr, addrlen);
-//   }
-//   close(fd);
-// }
